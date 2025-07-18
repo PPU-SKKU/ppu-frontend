51e4b0c95d847fa7e88da79ce1abcad63d3bd4e6
Minjae Oh <ominj98@gmail.com>
2025-07-18 13:44:39 +0900

    feat: App 컴포넌트에 SafeAreaView 래퍼 추가

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import OppuScreen from './src/screens/OppuScreen';
 import AddScreen from './src/screens/AddScreen';
 import ArchiveScreen from './src/screens/ArchiveScreen';
 import MyScreen from './src/screens/MyScreen';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const Tab = createBottomTabNavigator();
 
@@ -22,20 +23,17 @@ function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <>
-        <NavigationContainer>
-          <Tab.Navigator tabBar={props => <BottomTabBar {...props} />}>
-            <Tab.Screen name="HOME" component={HomeScreen} />
-            <Tab.Screen name="OPPU" component={OppuScreen} />
-            <Tab.Screen name="ADD" component={AddScreen} />
-            <Tab.Screen name="ARCHIVE" component={ArchiveScreen} />
-            <Tab.Screen name="MY" component={MyScreen} />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator tabBar={props => <BottomTabBar {...props} />}>
+          <Tab.Screen name="HOME" component={HomeScreen} />
+          <Tab.Screen name="OPPU" component={OppuScreen} />
+          <Tab.Screen name="ADD" component={AddScreen} />
+          <Tab.Screen name="ARCHIVE" component={ArchiveScreen} />
+          <Tab.Screen name="MY" component={MyScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
---
+++ src/components/common/BottomTabBar/styles.ts
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+  tab: {
+    flex: 1,
+    flexDirection: 'column',
+    gap: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    fontSize: 24,
+    color: '#A3A9B6',
+  },
+  addCircle: {
+    backgroundColor: '#34618E',
+    borderRadius: 100,
+    width: 56,
+    height: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addText: {
+    color: '#fff',
+    fontSize: 32,
+    includeFontPadding: false,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  label: {
+    color: '#6B7280',
+    fontSize: 14,
+  },
+});
+
+export default styles;
