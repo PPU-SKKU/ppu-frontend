627600ee48eafee82ece420f13a46a57dd5ffc50
Minjae Oh <ominj98@gmail.com>
2025-07-28 18:58:27 +0900

    feat: GeneralButton 컴포넌트 예시 코드 추가

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -12,6 +12,18 @@ const HomeScreen: React.FC = () => (
       backgroundColor="#34618E"
       textColor="#fff"
     ></GeneralButton>
+    <GeneralButton
+      size="medium"
+      label="버튼"
+      backgroundColor="#D6E4F7"
+      textColor="#3B4858"
+    ></GeneralButton>
+    <GeneralButton
+      size="small"
+      label="버튼"
+      backgroundColor="#ECEEF4"
+      textColor="#1C1B1F"
+    ></GeneralButton>
   </View>
 );
 
