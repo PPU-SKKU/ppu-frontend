fb64b4e27091327bd090c42c0c09f509043ad0fe
Yehyun Jo <81362348+JoYehyun99@users.noreply.github.com>
2025-08-19 15:41:12 +0900

    [CHORE] 코드 설정 수정 (#11)
    * chore: iOS 카메라 및 사진 라이브러리 접근 권한 설정 추가
    
    * chore: Android 카메라 및 사진 라이브러리 접근 권한 설정 추가
    
    * chore: image-picker 패키지 추가
    
    * chore: 안드로이드 Gradle 버전 설정 변경
    
    * feat: 갤러리에서 이미지 여러 장 선택 기능 구현
    
    * chore: json 파일 수정
    
    * chore: eslint 설정 완화
    
    * chore: eslint 설정 완화
    
    * chore: iOS 빌드 에러 수정
    
    * chore: iOS 빌드 에러 수정
    
    * chore: iOS 빌드 시뮬레이터 버전 수정
    
    * chore: iOS 빌드 OS 버전 변경
    
    * chore: iOS 빌드 수정
    
    * chore: iOS 빌드 디바이스 OS 버전 가져오기 수정
    
    * chore: iOS key값 오류 수정 (제발)
    
    * chore: iOS 빌드 로그 찍어보기
    
    * chore: iOS 시뮬레이터 + OS 찾기 제발 ...
    
    * chore: iOS ci 로그 찍기
    
    * chore: 로그 찍기 수정
    
    * chore: ios 빌드 제발요
    
    * chore: 괄호 및 공백 제거
    
    * chore: 하드 코딩 버전
    
    * chore: 사용 가능한 OS 로그 찍기
    
    * chore: 하드 코딩 값 넣기
    
    * chore: SDKROOT 지정
    
    * chore: 하드 코딩 버전 값 수정
    
    * chore: OS 버전만 추출
    
    * chore: bash 설정
    
    * chore: sdk 출력 로그
    
    * chore: settings 파일 추가
    
    * chore: iphone 버전 수정
    
    * chore: 로그 찍기
    
    * chore: 한계다..
    
    * chore: sdk 문자열 파싱 수정
    
    * chore: 자동으로 설치된 iOS 시뮬레이터와 SDK 버전을 감지하여 빌드
    
    * chore: 제발
    
    * chore: 들여쓰기 수정
    
    * chore: 다시 한 번 하드 코딩
    
    * chore: latest 값 수정
    
    * chore: mac-os 버전 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'no-undef': 'error',
+    'no-unreachable': 'error',
+    'no-dupe-keys': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-native/no-inline-styles': 'error',
+    'react-native/no-raw-text': 'error',
+
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    'no-unused-vars': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
 };
