f073e3bbb689cf11bbe3246507dc66cc54498631
Minjae Oh <ominj98@gmail.com>
2025-08-20 21:24:51 +0900

    chore: 사용하지 않는 컴포넌트 제거

--- src/screens/ArchiveScreen/index.tsx
+++ src/screens/ArchiveScreen/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
-import Button from '../../components/Buttons';
 
 const ArchiveScreen: React.FC = () => {
   const [perfumeReviewList, setPerfumeReviewList] = useState<any[]>([]);
@@ -11,12 +10,6 @@ const ArchiveScreen: React.FC = () => {
       {perfumeReviewList.length === 0 ? (
         <View style={styles.container}>
           <Text>나만의 시향기를 작성해보세요!</Text>
-          <Button
-            size="large"
-            label="시향 작성하기"
-            backgroundColor="#34618E"
-            textColor="#fff"
-          ></Button>
         </View>
       ) : (
         <View>
