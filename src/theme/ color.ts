c8334ddb4a4516d8053d65e8582184cfe9977034
Minjae Oh <ominj98@gmail.com>
2025-08-26 13:57:48 +0900

    chore: theme/colors.ts 추가

---
+++ src/theme/ color.ts
@@ -0,0 +1,25 @@
+const colors = {
+  // Black & White
+  black: '#000000',
+  white: '#FFFFFF',
+
+  // Grey scale (base #242424 with alpha steps)
+  grey4: 'rgba(36, 36, 36, 0.04)', // grey-4
+  grey8: 'rgba(36, 36, 36, 0.08)', // grey-8
+  grey12: 'rgba(36, 36, 36, 0.12)', // grey-12
+  grey31: 'rgba(36, 36, 36, 0.31)', // grey-31
+  grey39: 'rgba(36, 36, 36, 0.39)', // grey-39
+  grey54: 'rgba(36, 36, 36, 0.54)', // grey-54
+  grey100: '#242424', // grey 100%
+
+  // White scale (#FFFFFF with alpha steps)
+  white8: 'rgba(255, 255, 255, 0.08)', // White-8
+  white20: 'rgba(255, 255, 255, 0.20)', // White-20
+  white32: 'rgba(255, 255, 255, 0.32)', // White-32
+  white40: 'rgba(255, 255, 255, 0.40)', // White-40
+  white70: 'rgba(255, 255, 255, 0.70)', // White-70
+  white80: 'rgba(255, 255, 255, 0.80)', // White-80
+  white100: '#FFFFFF', // White 100%
+};
+
+export default colors;
