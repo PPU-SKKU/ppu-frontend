c410dfd367b3266ca5a50a7bade12f37ec6532af
JoYehyun <jo990714@naver.com>
2025-08-28 13:13:28 +0900

    chore: root 네비게이터 정의

---
+++ src/navigators/RootStackNavigator.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainTabNavigator from './MainTabNavigator';
+import CreateReviewScreen from '../screens/CreateReviewScreen';
+import { RootStackParamList } from '../types/navigationTypes';
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+const RootStackNavigator = () => {
+  return (
+    <RootStack.Navigator screenOptions={{ headerShown: false }}>
+      <RootStack.Screen name="Main" component={MainTabNavigator} />
+      {/* <RootStack.Screen name="CreateOppu" component={OppuWriteScreen} /> */}
+      <RootStack.Screen name="CreateReview" component={CreateReviewScreen} />
+    </RootStack.Navigator>
+  );
+};
+
+export default RootStackNavigator;
