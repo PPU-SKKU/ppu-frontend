f31daa514c6783565c761d42f936e72fce15578f
JoYehyun <jo990714@naver.com>
2025-08-28 13:12:37 +0900

    chore: 네비게이션 스택 타입 정의

---
+++ src/types/navigationTypes.ts
@@ -0,0 +1,32 @@
+export type RootStackParamList = {
+  Main: undefined;
+  CreateOppu: undefined;
+  CreateReview: undefined;
+};
+
+/* 추후 로그인 도입 시
+export type AuthStackParamList = {
+  Login: undefined;
+  SignUp: undefined;
+};
+*/
+
+export type HomeStackParamList = {
+  Home: undefined;
+};
+
+export type OppuStackParamList = {
+  Oppu: undefined;
+};
+
+export type AddStackParamList = {
+  Add: undefined;
+};
+
+export type ArchiveStackParamList = {
+  Archive: undefined;
+};
+
+export type MyStackParamList = {
+  My: undefined;
+};
