3dcdb3b6c37f8f3b4ca04949ee7b52e0a46004d4
JoYehyun <jo990714@naver.com>
2025-08-28 21:46:03 +0900

    feat: photoSlot 컴포넌트 구현

---
+++ src/screens/CreateOppuScreen/components/PhotoSlot/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Image, Pressable } from 'react-native';
+import styles from './styles';
+import IconButton from '../IconButton';
+import colors from '../../../../theme/color';
+
+interface PhotoSlotProps {
+  image?: string;
+  isAddButton?: boolean;
+  onPick?: () => void;
+  onDelete?: () => void;
+}
+
+const PhotoSlot: React.FC<PhotoSlotProps> = ({
+  image,
+  isAddButton = true,
+  onPick = () => {},
+  onDelete = () => {},
+}) => {
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.content} onPress={onPick}>
+        {image && (
+          <Image
+            source={{ uri: image }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        )}
+        {isAddButton && !image && (
+          <IconButton icon="plus" color={colors.white} style={styles.button} />
+        )}
+      </Pressable>
+
+      {image && (
+        <IconButton
+          icon="x"
+          onPress={onDelete}
+          color={colors.white}
+          style={styles.deleteButton}
+        />
+      )}
+    </View>
+  );
+};
+
+export default PhotoSlot;
