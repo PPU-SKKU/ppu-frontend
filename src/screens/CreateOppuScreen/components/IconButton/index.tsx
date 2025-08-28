447d636e810f5332c2467766cd4465a154c693bc
JoYehyun <jo990714@naver.com>
2025-08-28 17:31:44 +0900

    feat: 아이콘 버튼 비활성화 기능 추가

--- src/screens/CreateOppuScreen/components/IconButton/index.tsx
+++ src/screens/CreateOppuScreen/components/IconButton/index.tsx
@@ -7,6 +7,7 @@ import styles from './styles';
 interface IconButtonProps {
   icon: 'x' | 'plus' | 'minus';
   onPress: () => void;
+  disabled?: boolean;
   size?: number;
   color?: string;
   style?: ViewStyle;
@@ -15,6 +16,7 @@ interface IconButtonProps {
 const IconButton: React.FC<IconButtonProps> = ({
   icon,
   onPress,
+  disabled = false,
   size = 20,
   color = colors.black,
   style,
@@ -22,6 +24,7 @@ const IconButton: React.FC<IconButtonProps> = ({
   return (
     <Pressable
       onPress={onPress}
+      disabled={disabled}
       style={({ pressed }) => [
         {
           borderRadius: 22,
@@ -29,12 +32,17 @@ const IconButton: React.FC<IconButtonProps> = ({
           justifyContent: 'center',
           alignItems: 'center',
           alignSelf: 'center',
+          backgroundColor: disabled ? 'transparent' : colors.white,
         },
         style,
         pressed && { opacity: 0.7 },
       ]}
     >
-      <Feather name={icon} size={size} color={color} />
+      <Feather
+        name={icon}
+        size={size}
+        color={disabled ? colors.grey39 : color}
+      />
     </Pressable>
   );
 };
