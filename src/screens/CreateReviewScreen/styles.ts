070a9af5718905640bd3c8ad851fe9b932e751ec
Minjae Oh <ominj98@gmail.com>
2025-08-29 17:43:24 +0900

    feat: 시향기 작성 화면 PerfumeProfile 컴포넌트화

--- src/screens/CreateReviewScreen/styles.ts
+++ src/screens/CreateReviewScreen/styles.ts
@@ -16,57 +16,6 @@ const styles = StyleSheet.create({
     gap: 20,
     backgroundColor: colors.lightGrey,
   },
-  imageBackgroundContainer: {
-    alignSelf: 'stretch',
-    aspectRatio: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden',
-  },
-  perfumeProfileContainer: {
-    marginTop: -120,
-    alignSelf: 'stretch',
-    gap: 20,
-    flexDirection: 'column',
-    paddingTop: 40,
-    paddingBottom: 20,
-    paddingHorizontal: 20,
-    backgroundColor: colors.white40,
-    // backdrop-filter: blur(4px); //
-  },
-  perfumeDetailContainer: {
-    alignSelf: 'stretch',
-    flexDirection: 'column',
-    alignItems: 'center',
-    gap: 12,
-  },
-  perfumeInfoSection: {
-    alignSelf: 'stretch',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  perfumeStatusTagSection: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 8,
-  },
-
-  perfumePreferenceContainer: {
-    alignSelf: 'stretch',
-    flexDirection: 'column',
-    alignItems: 'center',
-    gap: 16,
-  },
-  segmentedControlsContainer: {
-    alignSelf: 'stretch',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 4,
-    borderRadius: 22,
-    backgroundColor: colors.grey8,
-  },
 
   ratingContainer: {},
   ratingLabelSection: {},
