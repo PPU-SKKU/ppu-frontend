055ad15a7283222d26dff12cb4b3e6b988362aff
JoYehyun <jo990714@naver.com>
2025-09-05 11:18:38 +0900

    feat: 최상단에 GestureHandlerRootView 적용

--- App.tsx
+++ App.tsx
@@ -3,14 +3,17 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import RootStackNavigator from '../ppu-frontend/src/navigators/RootStackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <RootStackNavigator />
-      </NavigationContainer>
-    </SafeAreaView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer>
+          <RootStackNavigator />
+        </NavigationContainer>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
