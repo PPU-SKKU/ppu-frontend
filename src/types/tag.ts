45bb5066c44877b863c862b20f845ca4f84b21a4
JoYehyun <jo990714@naver.com>
2025-09-05 12:19:30 +0900

    feat: ui 전용 Tag 모델 정의

--- src/types/tag.ts
+++ src/types/tag.ts
@@ -1,5 +1,18 @@
+import { hexToRgba } from '../utils/\bcolorsUtils';
+
 export interface Tag {
   id: string;
-  label: string;
+  label?: string;
   color: string;
+}
+
+export interface UITag extends Tag {
+  backgroundColor: string;
+}
+
+export function mapTags(tags: Tag[]): UITag[] {
+  return tags.map(tag => ({
+    ...tag,
+    backgroundColor: hexToRgba(tag.color, 0.12),
+  }));
 }
